b6287941de05cedb05d511095203e26ec94d53fc
j3r0nimo <111940067+j3r0nimo@users.noreply.github.com>
2024-11-03 16:30:25 -0300

    Add files via upload

---
+++ tp4/datos.sql
@@ -0,0 +1,14 @@
+INSERT INTO Produccion (LineaProduccion, FechaProduccion, CantidadProducida) VALUES
+
+    ('alfajor Fantoche', '2024-03-01', 100),
+    ('alfajor Fantoche', '2024-03-01', 150),
+    ('alfajor Fantoche', '2024-03-02', 120),
+    ('alfajor Fantoche', '2024-03-02', 130),
+    ('cubiertas Bridgestone', '2024-03-01', 200),
+    ('cubiertas Bridgestone', '2024-03-01', 180),
+    ('cubiertas Bridgestone', '2024-03-02', 190),
+    ('cubiertas Bridgestone', '2024-03-02', 210),
+    ('rocklets', '2024-03-01', 300),
+    ('rocklets', '2024-03-01', 320),
+    ('rocklets', '2024-03-02', 310),
+    ('rocklets', '2024-03-02', 330);
